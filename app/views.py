f61ae300c21f88f46a20f75ddb2cb629f78d831d
Kakha Svanidze <kakha.svanidze.22@pnu.edu.ua>
2024-10-22 10:22:03 +0300

    added blueprint with posts

--- app/views.py
+++ app/views.py
@@ -8,3 +8,23 @@ def main():
 @app.route('/resume')
 def resume():
     return render_template('resume.html', title='My resume')
+
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+
+    return render_template("home.html", agent=agent)
+
+@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", None, int)   
+
+    return render_template("hi.html",name=name, age=age)
+
+@app.route("/admin")
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    print(to_url)
+    return redirect(to_url)
